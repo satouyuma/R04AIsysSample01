0cd91d545bb5f1beb819d312f26a66b47e5f1354
200090 <200090@e0505-09.JC-21.local>
2022-10-24 14:35:26 +0900

    Sentiment.javaが動いた

--- src/main/java/jp/jc21/t/yoshizawa/WEB01/Sentiment.java
+++ src/main/java/jp/jc21/t/yoshizawa/WEB01/Sentiment.java
@@ -9,28 +9,28 @@ import java.util.Map;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
-public class Json05 {
+public class Sentiment {
 
 	public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
-		Language message = getLanguage("Stepover Toehold With Facelock");
+		Senti message = getSentiment("Stepover Toehold With Facelock");
 		if (message != null) {
-			System.out.println(message.documents[0].detectedLanguage.name);
+			System.out.println(message.documents[0].confidenceScores.positive);
 		}
 	}
 
-	static Language getLanguage(String s) throws IOException, URISyntaxException, InterruptedException {
+	static Senti getSentiment(String s) throws IOException, URISyntaxException, InterruptedException {
 		Gson gson = new Gson();
 
-		String url = "https://r04jk3b07-text.cognitiveservices.azure.com/" + "text/analytics/v3.0/languages";
+		String url = "https://r04jk3b07-text.cognitiveservices.azure.com/" + "text/analytics/v3.0/sentiment";
 		Map<String, String> map = new HashMap<>();
 		map.put("Ocp-Apim-Subscription-Key", "686e7194f8bf4bbc9baae84352f33cb1");
 
-		Docs doc = new Docs();
+		Doc doc = new Doc();
 		doc.id = "1";
 		doc.text = s;
 
-		Source src = new Source();
-		src.documents = new Docs[1];
+		Sour src = new Sour();
+		src.documents = new Doc[1];
 		src.documents[0] = doc;
 
 		String jsonData = new Gson().toJson(src);
@@ -38,9 +38,9 @@ public class Json05 {
 		InetSocketAddress proxy = new InetSocketAddress("172.17.0.2", 80);
 
 		JsonReader reader = WebApiConnector.postJsonReader(url, proxy, map, jsonData);
-		Language message = null;
+		Senti message = null;
 		if (reader != null) {
-			message = gson.fromJson(reader, Language.class);
+			message = gson.fromJson(reader, Senti.class);
 			reader.close();
 		}
 		return message;
@@ -48,25 +48,27 @@ public class Json05 {
 
 }
 
-class Language {
-	Documents[] documents;
+class Senti {
+	Document[] documents;
 	String[] errors;
 	String modelVersion;
 }
 
-class Documents {
-	DetectedLanguage detectedLanguage;
+class Document {
+	ConfidenceScores confidenceScores;
 }
 
-class DetectedLanguage {
-	String name;
+class ConfidenceScores {
+	double positive;
+	double neutral;
+	double negative;
 }
 
-class Source {
-	Docs[] documents;
+class Sour {
+	Doc[] documents;
 }
 
-class Docs {
+class Doc {
 	String id;
 	String text;
 }
